58cb7911256074ea3b84f60a9ba6c775dccc9e7f
Snowballe <62845660+Snowballe@users.noreply.github.com>
2022-01-22 18:16:00 +0100

    Changing directory names + trying to make wpf work

---
+++ HuitMortsSixBlesses.WPF/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using System.Windows;
+
+namespace HuitMortsSixBlesses.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+        }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //Ca serait mieux de mettre l'URL dans un fichier de config plutôt qu'en dur ici
+            
+            //var clientApi = new Client("https://localhost:44319/", new HttpClient());
+
+            //le async et le await c'est de la programmation asynchrone en C#
+            
+            //var triangles = await clientApi.PanierAdhAllAsync();
+
+            
+        }
+
+        private void BtnSignin(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnToWebsite(object sender,RoutedEventArgs e)
+        {
+            string url = "https://www.google.com";
+            Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
+        }
+    }
+}
---
+++ HuitMortsSixBlesses.Web/Controllers/TrianglesController.cs
@@ -0,0 +1,53 @@
+﻿
+using HuitMortsSixBlesses.Web.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HuitMortsSixBlesses.Web.Controllers
+{
+    public class TrianglesController : Controller
+    {
+        private IHuitMortsSixBlessesService service;
+
+        public TrianglesController(IHuitMortsSixBlessesService srv)
+        {
+            service = srv;
+        }
+
+        public IActionResult Index()
+        {
+            var triangles = service.GetAllTriangles();
+            return View(triangles);
+        }
+
+        public IActionResult Ajouter()
+        {
+            return View(new TriangleVM());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Ajouter([Bind("X1,Y1,X2,Y2,X3,Y3")]TriangleVM vm)
+        {
+            if (ModelState.IsValid)
+            {
+                //je transforme mon viewmodel en objet métier
+                var tri = new Panier_Fournisseur(
+                        new Ligne(vm.X1.Value,vm.Y1.Value)
+                        ,new Ligne(vm.X2.Value, vm.Y2.Value)
+                        ,new Ligne(vm.X3.Value, vm.Y3.Value)
+                    );
+                //j'appelle le service
+                service.Insert(tri);
+
+                //je retourne à la liste des triangles
+                return RedirectToAction("Index");
+            }
+
+            return View(vm);
+        }
+    }
+}
